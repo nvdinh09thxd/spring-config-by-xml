84aaea8599bb98bd9261f0561c0ac7f240da54d2
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-11-05 10:36:47 +0700

    rename project & url
    x
    
    x
    
    x

--- src/main/java/com/springmvc/controllers/IndexController.java
+++ src/main/java/com/springmvc/controllers/IndexController.java
@@ -1,6 +1,5 @@
 package com.springmvc.controllers;
 
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("home")
 public class IndexController {
 
-	@GetMapping("index")
+	@GetMapping("hello")
 	public String sayHello() {
 		return "<p style='color: blue'>Hello World!</p>";
 	}
 	
-	public String sayGoodbye(ModelMap map) {
-		map.addAttribute("msg", "Nguyen Van Dinh");
-		return "hello";
+	@GetMapping("bye")
+	public String sayGoodbye() {
+		return "<p style='color: violet'>Goodbye!</p>";
 	}
 }
